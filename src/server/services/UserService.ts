ea2eba3b711af50fdf4f7cc568deee7ad7d35f4d
thanosbousios <bousiosthan@gmail.com>
2023-12-28 13:25:24 +0200

    simplify

--- src/server/services/UserService.ts
+++ src/server/services/UserService.ts
@@ -49,41 +49,42 @@ export default class UserService extends AbstractService {
 
   private async _addSystemUserRequestHandler(req: Request, res: Response): Promise<void> {
     const user = await this.osuClient.getUserById(parseInt(req.body['id']));
-    if (user) {
-      let result: LeaderboardUser[] = [];
-      let i = 1;
-      let leaderboard = await this.osuClient.getCountryLeaderboard({ country: user.country_code, 'cursor[page]': i.toString() });
-      do {
-        i++;
-        console.log(leaderboard?.ranking.map((x) => x.user.username));
-        if (!leaderboard) {
-          i--;
-          continue;
-        }
-        result = result.concat(leaderboard.ranking);
-        leaderboard = await this.osuClient.getCountryLeaderboard({ country: user.country_code, 'cursor[page]': i.toString() });
-      } while (leaderboard?.cursor);
-
-      result.sort((a, b) => (a.ranked_score < b.ranked_score ? 1 : -1));
-
-      const createdUser = await this.databaseClient.updateSystemUser(user, result.findIndex((user) => user.user.id === parseInt(req.body['id'])) + 1);
-
-      await this.databaseClient.updateLeaderboard(result.slice(0, 200));
-
-      res.status(200).json({
+    if (!user) {
+      res.status(404).json({
         meta: {
-          status: 200,
+          status: 404,
         },
-        data: createdUser,
       });
-
       return;
     }
-    res.status(404).json({
+
+    let result: LeaderboardUser[] = [];
+    let i = 1;
+    let leaderboard = await this.osuClient.getCountryLeaderboard({ country: user.country_code, 'cursor[page]': i.toString() });
+    do {
+      i++;
+      console.log(leaderboard?.ranking.map((x) => x.user.username));
+      if (!leaderboard) {
+        i--;
+        continue;
+      }
+      result = result.concat(leaderboard.ranking);
+      leaderboard = await this.osuClient.getCountryLeaderboard({ country: user.country_code, 'cursor[page]': i.toString() });
+    } while (leaderboard?.cursor);
+
+    result.sort((a, b) => (a.ranked_score < b.ranked_score ? 1 : -1));
+
+    const createdUser = await this.databaseClient.updateSystemUser(user, result.findIndex((user) => user.user.id === parseInt(req.body['id'])) + 1);
+
+    await this.databaseClient.updateLeaderboard(result.slice(0, 200));
+
+    res.status(200).json({
       meta: {
-        status: 404,
+        status: 200,
       },
+      data: createdUser,
     });
+
   }
 
   private async _getUserByIdRequestHandler(req: Request, res: Response): Promise<void> {
