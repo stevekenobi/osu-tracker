c96e756aee8a54b0d3125bb432e3e8795ae4eed9
thanosbousios <bousiosthan@gmail.com>
2024-02-17 11:59:13 +0200

    some iteration of tests

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,10 @@
 const config = {
   verbose: true,
   setupFiles: ['./tests/setup.js'],
-  collectCoverage: false,
+  collectCoverage: true,
+  coverageDirectory: './coverage',
+  coverageProvider: 'v8',
+  collectCoverageFrom: ['src/**/*.{js,jsx}']
 };
 
 module.exports = config;
