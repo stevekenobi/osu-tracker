8ab24cedb0e30afe661363fbeb34b27c940ac65b
thanosbousios <bousiosthan@gmail.com>
2024-03-12 10:29:45 +0200

    fix front sheet of ages spreadsheet

--- src/server/helpers/ages.ts
+++ src/server/helpers/ages.ts
@@ -16,14 +16,20 @@ export async function syncAgesSheet(): Promise<void> {
         id: b.id,
       }))
       .sort((a, b) => (a.age > b.age ? 1 : -1));
+    const beatmapsOfYoungestAge = beatmapsOfSameAge(beatmapAges, 'youngest');
+    const beatmapsOfOldestAge = beatmapsOfSameAge(beatmapAges, 'oldest');
     ages.push({
       Year: year,
       'Average Age': numeral(beatmapAges.reduce((sum, x) => sum + x.age, 0) / beatmapAges.length).format('0,0'),
       'Oldest Age': numeral(beatmapAges.at(-1)!.age).format('0,0'),
       'Youngest Age': numeral(beatmapAges[0]!.age).format('0,0'),
-      'Oldest Map': createBeatmapsetLinkFromId(beatmapAges.at(-1)!.id),
-      'Youngest Map': createBeatmapsetLinkFromId(beatmapAges[0]!.id),
+      'Oldest Map': beatmapsOfYoungestAge > 1 ? 'multiple beatmapsets' : createBeatmapsetLinkFromId(beatmapAges.at(-1)!.id),
+      'Youngest Map': beatmapsOfOldestAge > 1 ? 'multiple beatmapsets' : createBeatmapsetLinkFromId(beatmapAges[0]!.id),
     });
   }
   await TrackerServer.getSheetClient().updateAges(ages);
+}
+
+export function beatmapsOfSameAge(beatmaps: { age: number, id: number }[], type: 'youngest' | 'oldest'): number {
+  return beatmaps.filter(b => b.age === beatmaps.at(type === 'youngest' ? -1 : 0)!.age).length;
 }
