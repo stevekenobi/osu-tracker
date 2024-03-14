1c97910b984001323761129f1db89b4436461fd3
thanosbousios <bousiosthan@gmail.com>
2024-03-14 11:47:02 +0200

    collections include yearly maps

--- src/server/helpers/collection.ts
+++ src/server/helpers/collection.ts
@@ -1,23 +1,37 @@
 import type { OsuCollection } from '../../client/OsuCollection';
+import { getYearsUntilToday } from '../../utils';
 import TrackerServer from '../server';
 
 export async function getCollections(): Promise<OsuCollection[]> {
   const collections: OsuCollection[] = [];
 
-  collections.push(await getCollection('a-ranks'));
-  collections.push(await getCollection('no-score'));
-  collections.push(await getCollection('problematic'));
-  collections.push(await getCollection('non-sd'));
-  collections.push(await getCollection('dt'));
-  collections.push(await getCollection('sub-optimal'));
+  collections.push(await getUnfinishedCollection('a-ranks'));
+  collections.push(await getUnfinishedCollection('no-score'));
+  collections.push(await getUnfinishedCollection('problematic'));
+  collections.push(await getUnfinishedCollection('non-sd'));
+  collections.push(await getUnfinishedCollection('dt'));
+  collections.push(await getUnfinishedCollection('sub-optimal'));
+
+  for (const year of getYearsUntilToday()) {
+    collections.push(await getYearlyCollection(year));
+  }
 
   return collections;
 }
 
-export async function getCollection(option: 'no-score' | 'a-ranks' | 'problematic' | 'non-sd' | 'dt' | 'sub-optimal'): Promise<OsuCollection> {
+export async function getUnfinishedCollection(option: 'no-score' | 'a-ranks' | 'problematic' | 'non-sd' | 'dt' | 'sub-optimal'): Promise<OsuCollection> {
   const beatmaps = await TrackerServer.getDatabaseClient().getUnfinishedBeatmaps(option);
   return {
     name: option,
+    beatmapCount: beatmaps.length,
+    beatmaps: beatmaps.map((b) => b.checksum),
+  };
+}
+
+export async function getYearlyCollection(year: string): Promise<OsuCollection> {
+  const beatmaps = (await TrackerServer.getDatabaseClient().getBeatmapsOfYear(year)).filter(b => !b.score);
+  return {
+    name: year,
     beatmapCount: beatmaps.length,
     beatmaps: beatmaps.map((b) => b.checksum),
   };
